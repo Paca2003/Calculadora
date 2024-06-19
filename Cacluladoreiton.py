33fd90e3da97ed054495af4227927f118d2090dc
Paca2003 <cr.barrientoss@duocuc.cl>
2024-06-19 15:20:09 -0400

    Update Cacluladoreiton.py
    Version multiplicacion y division

--- Cacluladoreiton.py
+++ Cacluladoreiton.py
@@ -2,4 +2,13 @@
 # Crear una calculadora de manera colaborativa
 
 
+def Mult(num1,num2):
+    return num1*num2;
+
+def Div(num1,num2):
+    try:
+        return num1/num2
+    except ZeroDivisionError:
+        return "SYNTAX ERROR"
+
 
